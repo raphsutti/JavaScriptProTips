f76443e0a0127df30ec08b2d4f62b9a994c8f7e3
Raph <rsuttiyotin@seek.com.au>
2021-02-07 11:17:09 +1100

    ts config target

--- objectEntires.ts
+++ objectEntires.ts
@@ -5,11 +5,11 @@ const object1 = {
   b: 42,
 };
 
+console.log(Object.entries(object1)); // [ [ 'a', 'somestring' ], [ 'b', 42 ] ]
+
 for (const [key, value] of Object.entries(object1)) {
   console.log(`${key}: ${value}`);
 }
-
-// expected output:
 // "a: somestring"
 // "b: 42"
 // order is not guaranteed
